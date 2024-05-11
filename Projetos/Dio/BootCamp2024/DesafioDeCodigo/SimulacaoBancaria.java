0edc447c452f12888b58ffd7ecff047a5666ebd6
r_souza <Rafaelrosod@outlook.com>
2024-05-10 23:51:22 -0300

    Desafio de cód. 1/5, concluído.

---
+++ Projetos/Dio/BootCamp2024/DesafioDeCodigo/SimulacaoBancaria.java
@@ -0,0 +1,39 @@
+package Projetos.Dio.BootCamp2024.DesafioDeCodigo;
+
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            double saldo = 0;
+            int opcao;
+            do {
+                opcao = Integer.parseInt(scanner.nextLine());
+                switch (opcao) {
+                    case 1:
+                        double valorDeposito = Double.parseDouble(scanner.nextLine());
+                        saldo += valorDeposito;
+                        System.out.println("Saldo atual: " + saldo);
+                        break;
+                    case 2:
+                        double valorSaque = Double.parseDouble(scanner.nextLine());
+                        if (valorSaque <= saldo) {
+                            saldo -= valorSaque;
+                            System.out.println("Saldo atual: " + saldo);
+                        } else {
+                            System.out.println("Saldo insuficiente");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Saldo atual: " + saldo);
+                        break;
+                    case 0:
+                        System.out.println("Programa encerrado.");
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcao != 0);
+        }
+    }
+}
